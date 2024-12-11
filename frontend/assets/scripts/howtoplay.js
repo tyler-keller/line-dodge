253b88440ee72adb979426b3aa611b75482508fa
whu09 <whu@mines.edu>
2024-12-11 15:15:11 -0700

    improved how to play menu

--- frontend/assets/scripts/howtoplay.js
+++ frontend/assets/scripts/howtoplay.js
@@ -2,6 +2,10 @@ var modal = document.getElementById("how-to-play");
 var btn = document.getElementById("open-btn");
 var btn2 = document.getElementById("open-btn2");
 var span = document.getElementsByClassName("close")[0];
+var page1 = document.getElementById("htp-page-1");
+var page2 = document.getElementById("htp-page-2");
+var forwardBtn = document.getElementById("htp-forward-button");
+var backBtn = document.getElementById("htp-back-button");
 
 const clickSound = new Audio('assets/Sounds/click.wav');
 clickSound.preload = 'auto';
@@ -23,4 +27,15 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+forwardBtn.onclick = function() {
+  clickSound.play();
+  page1.style.display = "none";
+  page2.style.display = "block";
+}
+backBtn.onclick = function() {
+  clickSound.play();
+  page1.style.display = "block";
+  page2.style.display = "none";
 }
